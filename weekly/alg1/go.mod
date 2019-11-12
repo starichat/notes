9ac97639cc80859b1f5327969aa6b0090bae04ef
starichat <starichat@163.com>
2019-11-12 22:52:50 +0800

    add commit

---
+++ weekly/alg1/go.mod
@@ -0,0 +1,3 @@
+module alg1
+
+go 1.12
---
+++ weekly/alg1/main.go
@@ -0,0 +1,24 @@
+package main
+
+// 用go实现栈
+type StackMax struct {
+	data int
+}
+
+var stackMax StackMax
+
+// 用栈实现O1的登陆
+func push(newNum int) {
+	if stackMax == nil {
+
+	}
+}
+
+// get the maxest value from a array
+func getMax(array []int) {
+
+}
+
+func main() {
+
+}
